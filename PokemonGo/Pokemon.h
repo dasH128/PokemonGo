5c4ce3ce2bf0ff1e60627d871fb0223ad317f047
dasH128 <rojas71421@gmail.com>
2016-11-18 01:59:39 -0500

    2

--- PokemonGo/Pokemon.h
+++ PokemonGo/Pokemon.h
@@ -1,8 +1,20 @@
 #pragma once
+using namespace System::Drawing;
+
 class CPokemon
 {
+private:
+	int id;
+	char *nombre;
+	int tipo;
+	int atk1, atk2, atk3, atk4;
+
+
 public:
-	CPokemon();
+	CPokemon(int id);
 	~CPokemon();
+
+	void CrearPokemon();
+	void Pintar();
 };
 
